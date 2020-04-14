3a93b84b413a2b446e9246be38acf06c3f5f8965
Kristina Zakharova <zakharova.kr@gmail.com>
2020-04-14 13:46:20 -0400

    added a new line

--- HelloWorld/src/main/java/Hello.java
+++ HelloWorld/src/main/java/Hello.java
@@ -7,5 +7,6 @@ package com.zakharovakr.helloworld; // Use YOUR Group Id value here instead of c
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("hi, I'm Kristina");
     }
 }
