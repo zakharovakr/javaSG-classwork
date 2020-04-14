36003a4c66304599eb9a222d570667807eff5531
Kristina Zakharova <zakharova.kr@gmail.com>
2020-04-14 13:49:43 -0400

    adding new lines

--- HelloWorld/src/main/java/Hello.java
+++ HelloWorld/src/main/java/Hello.java
@@ -8,5 +8,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("hi, I'm Kristina");
+        System.out.println("hi there");
     }
 }
