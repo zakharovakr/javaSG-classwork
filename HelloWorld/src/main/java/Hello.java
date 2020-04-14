194c1f742b169d64d6999ccbbd5c0a8476193d98
Kristina Zakharova <zakharova.kr@gmail.com>
2020-04-14 13:38:17 -0400

    pushing with NetBeans

---
+++ HelloWorld/src/main/java/Hello.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * @author kristinazakharova
+ */
+package com.zakharovakr.helloworld; // Use YOUR Group Id value here instead of com.tsg
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
