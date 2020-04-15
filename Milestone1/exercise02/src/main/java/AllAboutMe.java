6cc0044f1d7cbd1e66113450740468a41bad7459
Kristina Zakharova <zakharova.kr@gmail.com>
2020-04-15 19:00:08 -0400

    completed exercise02 (lesson04)

---
+++ Milestone1/exercise02/src/main/java/AllAboutMe.java
@@ -0,0 +1,24 @@
+public class AllAboutMe {
+    public static void main(String[] args) {
+        //declare variables
+        String name, favFood, myCity, hobby;
+        int petsNumber, age;
+
+        //assign values
+        name = "Kristina";
+        favFood = "borsch";
+        myCity = "St. Petersburg, Russia";
+        hobby = "yoga";
+        petsNumber = 0;
+        age = 26;
+
+
+        //print out info
+        System.out.println("Hi! My name is " + name + ".");
+        System.out.println("I am " + age + " years old.");
+        System.out.println("I come from " + myCity + ".");
+        System.out.println("I have " + petsNumber + " pets.");
+        System.out.println("My favorite food is " + favFood + ".");
+        System.out.println("My hobby is practicing " + hobby + ".");
+    }
+}
---
+++ Milestone1/exercise02/src/main/java/BestAdderEver.java
@@ -0,0 +1,16 @@
+public class BestAdderEver {
+    public static void main(String[] args) {
+        int num1, num2, num3, sum;
+
+        num1 = 5;
+        num2 = 7;
+        num3 = 76;
+
+        sum = num1 + num2 + num3;
+
+        System.out.print(num1);
+        System.out.print("+" + num2);
+        System.out.print("+" + num3);
+        System.out.print("=" + sum);
+    }
+}
